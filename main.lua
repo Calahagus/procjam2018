039fccae46b29a512f1bd35c33a863891e89be91
Calahan Janik-Jones <caljj@me.com>
2018-10-21 16:04:00 -0400

    Reworked stage and pause management

--- main.lua
+++ main.lua
@@ -1,42 +1,67 @@
--- Stages
--- TODO: consider moving these functions into 
+pause_fade = 0
+pause_target = false
+pause_state = false
+
+pause_fadescale = 200
+pause_fademax = 100
+pause_fademin = 0
+
 stage_menu = require("scripts/stages/stageMenu")
 stage_vn = require("scripts/stages/stageVN")
 stage_rhythm = require("scripts/stages/stageRhythm")
-
--- universal_calls = { allows_pause, load, update, draw }
--- TODO: write a test script that verifies that every stage has all universal calls
-
--- Renderers
--- gui = require('scripts/gui/Gspot')
-
-stage = stage_menu
-pause = require("scripts/gui/renderPause")
+stage = nil
 
 function love.load()
-	stage_menu.load()
+	load_stage(stage_menu)
 end
 
 function love.update(dt)
-	if not (pause.state and stage.allows_pause) then
+	if not (pause_state and stage.allows_pause) then
 		stage.update(dt)
 	end
-	pause.update(dt)
-end
 
-function love.keypressed(key)
-	if key == 'escape' and stage.allows_pause then
-		pause.target = not pause.target
+	if (pause_target and pause_fade < pause_fademax) then
+		pause_fade = pause_fade + pause_fadescale * dt
+	elseif not pause_target and pause_fade > pause_fademin then
+		pause_fade = pause_fade - pause_fadescale * dt
+	end
+	pause_fade = math.min(pause_fade, pause_fademax)
+	pause_fade = math.max(pause_fade, pause_fademin)
+	if pause_fade > pause_fademin then 
+		pause_state = true 
+	else 
+		pause_state = false 
 	end
 end
 
 function love.draw()
 	stage.draw()
-	pause.draw()
+
+	if pause_state then
+		love.graphics.print("PAUSED",100,24)
+		-- TODO: make this better 
+	end
+	love.graphics.print(tostring(pause_target),0,0)
+	love.graphics.print(tostring(pause_fade),0,72)
+end
+
+function love.keypressed(key)
+	if key == 'escape' and stage.allows_pause then
+		pause_target = not pause_target
+	end
 end
 
 function load_stage(target_stage)
-	paused = false
+	-- TODO: add a fading-out function before doing the sharp cut to any of this stuff
+	-- TODO: add a pre-loader stage with a spinning little wheel or something. put init calls into a sub-function
+	target_stage.load()
 	stage = target_stage
-	stage.load()
+	pause_clear()
 end
+
+function pause_clear()
+	pause_state = false
+	pause_target = false
+	pause_fade = 0
+end
+
